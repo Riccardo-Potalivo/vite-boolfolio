f3ad3bd8f67e61a6f00d311c5e4769e978ea0038
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-21 08:51:44 +0100

    fix init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    Ciao !
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+
+  name: "App",
+
+  data() {
+    return{
+      store,
+      projects: []
+    };
+  },
+
+  methods: {
+    getAllProjects() {
+      axios
+        .get(store.apiUrl + "/projects").then((res) => {
+          console.log(res.data);
+          // this.projects = res.data.response;
+          console.log(this.projects);
+        })
+    }
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
